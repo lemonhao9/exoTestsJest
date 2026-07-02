268c3f7cb03bf33527e7e4142582960c430d6c3c
Lemon Hao <leohamon9@gmail.com>
2026-07-02 11:30:14 +0200

    c'est bon la ?

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const {app} = require('./app.js');
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
