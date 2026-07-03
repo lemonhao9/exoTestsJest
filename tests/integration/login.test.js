3eff9bd56ff22e419dfa52e302b867d08801b79e
Lemon Hao <leohamon9@gmail.com>
2026-07-03 09:24:04 +0200

    osmtest

--- tests/integration/login.test.js
+++ tests/integration/login.test.js
@@ -12,7 +12,7 @@ describe('POST /login', () => {
     it('should return status code 400 and error message for invalid email or invalid password', async () => {
         const response = await request(app)
             .post('/login')
-            .send({ email: 'test@example.com', password: 'password123' });
+            .send({ email: 'test@example.com', password: 'passwor' });
         expect(response.status).toBe(400);
         expect(response.body.message).toEqual('Invalid email or password');
     });
